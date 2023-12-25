ae9d694f1843e0f90535f450579011c6c9ec40b5
SaldNevo <artemkorobov4119@gmail.com>
2023-12-25 23:51:08 +0300

    random questions

--- js/main.js
+++ js/main.js
@@ -1,112 +1,123 @@
-"use strict";
+'use strict'
 //==========================================
-const $ = document.querySelector.bind(document);
+const $ = document.querySelector.bind(document)
+const quiz = $('.quiz')
+const warning = $('.warning')
+const btnNext = $('.quiz__next-btn')
+let questionNumber = 0
+let userScore = 0
+let count = 0
 
-const quiz = $(".quiz");
-const warning = $(".warning");
-const btnNext = $(".quiz__next-btn");
-let count = 0;
-let userScore = 0;
+let cheackNumber = []
+getRandomNumber()
 
+function getRandomNumber() {
+	count = Math.floor(Math.random() * questions.length)
 
+	console.log(count)
+	cheackNumber.push(count)
+	
+	for (let i = 0; i < cheackNumber.length; i++) {
+		if (count == cheackNumber[i] && i != cheackNumber.length - 1) {
+			count = Math.floor(Math.random() * questions.length)
+			cheackNumber.pop()
+			cheackNumber.push(count)
+			i = 0;
+		}
+	}
 
-if (typeof questions !== 'undefined' && questions.length > 0){
-  quiz.classList.remove('hidden');
-  showQuestions(count)
-}else{
-  warning.classList.remove("hidden");
+	return count
 }
 
-
-
-btnNext.addEventListener("click", nextQuestion);
-
-
-
-function showQuestions(index){
-  const imgBox = $(".quiz__top-img");
-  const title = $(".quiz__title");
-  const list = $(".quiz__list");
-  const total = $(".quiz__total")
-
-  imgBox.setAttribute("src", `${img[index]}`)
-
-  title.innerHTML = `${questions[index].question}`;
-
-  list.innerHTML = '';
-  questions[index].options.forEach(item => {
-    const text = `<li class = "quiz__option">${item}</li>`;
-    list.insertAdjacentHTML("beforeend",text)
-  });
-
-  const options = list.querySelectorAll(".quiz__option");
-  options.forEach(item => item.setAttribute("onclick", "optionSelected(this)"))
-
-  total.innerHTML = `${index + 1} из ${questions.length}`
+if (typeof questions !== 'undefined' && questions.length > 0) {
+	quiz.classList.remove('hidden')
+	showQuestions(count)
+} else {
+	warning.classList.remove('hidden')
 }
 
+btnNext.addEventListener('click', nextQuestion)
 
+function showQuestions(index) {
+	const imgBox = $('.quiz__top-img')
+	const title = $('.quiz__title')
+	const list = $('.quiz__list')
+	const total = $('.quiz__total')
 
+	imgBox.setAttribute('src', `${img[index]}`)
 
-function optionSelected(answer){
-  const userAnswer = answer.textContent;
-  const correctAnswer = questions[count].answer;
-  const options = document.querySelectorAll(".quiz__option")
-  const iconCorrect = "<span'>&#10004;</span>";
-  const iconIncorrect = "<span'>&#9940;</span>";
+	title.innerHTML = `${questions[index].question}`
 
-    if(userAnswer == correctAnswer){
-      userScore +=1;
-      answer.classList.add("correct")
-      answer.insertAdjacentHTML("beforeend", iconCorrect)
+	list.innerHTML = ''
+	questions[index].options.forEach(item => {
+		const text = `<li class = "quiz__option">${item}</li>`
+		list.insertAdjacentHTML('beforeend', text)
+	})
 
-      options.forEach(item => item.classList.add('disabled'))
-    }else{
-      answer.classList.add('incorrect')
-			answer.insertAdjacentHTML('beforeend', iconIncorrect)
+	const options = list.querySelectorAll('.quiz__option')
+	options.forEach(item => item.setAttribute('onclick', 'optionSelected(this)'))
 
-      options.forEach(item => {
-        if (item.textContent == correctAnswer) {
-          setTimeout(() => {
-            item.classList.add("correct")
-            item.insertAdjacentHTML("beforeend", iconCorrect)
-          }, 100)
-        }
-      })
-
-      options.forEach(item => item.classList.add("disabled"))
-    }
+	total.innerHTML = `${(questionNumber += 1)} из ${questions.length}`
 }
 
+function optionSelected(answer) {
+	const userAnswer = answer.textContent
+	const correctAnswer = questions[count].answer
+	const options = document.querySelectorAll('.quiz__option')
+	const iconCorrect = "<span'>&#10004;</span>"
+	const iconIncorrect = "<span'>&#9940;</span>"
 
+	if (userAnswer == correctAnswer) {
+		userScore += 1
+		answer.classList.add('correct')
+		answer.insertAdjacentHTML('beforeend', iconCorrect)
 
-function nextQuestion(){
-  const option = $(".quiz__option");
-  const result = $(".result");
-  const resultText = $(".result__text")
+		options.forEach(item => item.classList.add('disabled'))
+	} else {
+		answer.classList.add('incorrect')
+		answer.insertAdjacentHTML('beforeend', iconIncorrect)
 
-  if ((count + 1) == questions.length && option.classList.contains('disabled')){
-    result.classList.remove("hidden");
-    quiz.classList.add("hidden");
+		options.forEach(item => {
+			if (item.textContent == correctAnswer) {
+				setTimeout(() => {
+					item.classList.add('correct')
+					item.insertAdjacentHTML('beforeend', iconCorrect)
+				}, 100)
+			}
+		})
 
-    if(userScore <= 7 ){
-      resultText.innerHTML = `Отшиваем тебя <br> Правильно ${userScore} из ${questions.length}`
-    }else if (userScore <= 13){
-      resultText.innerHTML =
-				`Ты Чушпан, а не пацан! <br> Правильно ${userScore} из ${questions.length}`
-    }else if (userScore <= 18){
-      resultText.innerHTML =
-				`Эй Скорлупа!! <br> Правильно ${userScore} из ${questions.length}`
-    }else{
-      resultText.innerHTML = `Старшый!!! <br> Правильно ${userScore} из ${questions.length}`
-    }
-    return
-  }
+		options.forEach(item => item.classList.add('disabled'))
+	}
+}
 
-  if (option.classList.contains('disabled')){
-    count++;
-    showQuestions(count);
-  }else{
-    alert("Выберите один из вариантов ответа");
-  }
+function nextQuestion() {
+	const option = $('.quiz__option')
+	const result = $('.result')
+	const resultText = $('.result__text')
+
+	if (
+		questionNumber + 1 == questions.length &&
+		option.classList.contains('disabled')
+	) {
+		result.classList.remove('hidden')
+		quiz.classList.add('hidden')
+
+		if (userScore <= 7) {
+			resultText.innerHTML = `Отшиваем тебя <br> Правильно ${userScore} из ${questions.length}`
+		} else if (userScore <= 13) {
+			resultText.innerHTML = `Ты Чушпан, а не пацан! <br> Правильно ${userScore} из ${questions.length}`
+		} else if (userScore <= 18) {
+			resultText.innerHTML = `Эй Скорлупа!! <br> Правильно ${userScore} из ${questions.length}`
+		} else {
+			resultText.innerHTML = `Старшый!!! <br> Правильно ${userScore} из ${questions.length}`
+		}
+		return
+	}
+
+	if (option.classList.contains('disabled')) {
+		getRandomNumber()
+		showQuestions(count)
+	} else {
+		alert('Выберите один из вариантов ответа')
+	}
 }
